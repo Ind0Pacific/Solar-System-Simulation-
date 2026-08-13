4482f0267a87fb7bf5e2a70d09fd8f4dd69b1006
Ind0Pacific <vashishtdeepanshu87@gmail.com>
2026-08-13 22:14:54 +0530

    Updated models and added rotation.

--- project/openGL.cpp
+++ project/openGL.cpp
@@ -140,34 +140,50 @@ int main() {
     Sphere earth(36, 18, 0.5f);
 
     while (!glfwWindowShouldClose(window)) {
+    Sphere sun(36, 18, 2.0f);
+    Sphere earth(36, 18, 0.5f);
+
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    while (!glfwWindowShouldClose(window)) {
         glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        
         glUseProgram(shaderProgram);
+
+        float time = glfwGetTime();
+
         glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
         glm::mat4 view = glm::lookAt(
-            glm::vec3(0.0f, 15.0f, 15.0f),  
-            glm::vec3(0.0f, 0.0f, 0.0f),    
-            glm::vec3(0.0f, 1.0f, 0.0f)     
+            glm::vec3(0.0f, 15.0f, 15.0f), 
+            glm::vec3(0.0f, 0.0f, 0.0f),   
+            glm::vec3(0.0f, 1.0f, 0.0f)   
         ); 
 
+        float sunSpinAngle = time * 0.5f; 
         glm::mat4 sunModel = glm::mat4(1.0f); 
-        glm::mat4 sunMVP = projection * view * sunModel;
+        sunModel = glm::rotate(sunModel, sunSpinAngle, glm::vec3(0.0f, 1.0f, 0.0f)); 
         
+        glm::mat4 sunMVP = projection * view * sunModel;
         glUniformMatrix4fv(mvpLoc, 1, GL_FALSE, glm::value_ptr(sunMVP));
-        glUniform3f(colorLoc, 1.0f, 0.9f, 0.0f);
+        glUniform3f(colorLoc, 1.0f, 0.9f, 0.0f); 
         sun.draw();
 
+        float earthOrbitAngle = time * 1.0f; 
+        float earthSpinAngle = time * 3.0f;  
+
         glm::mat4 earthModel = glm::mat4(1.0f);
-        earthModel = glm::translate(earthModel, glm::vec3(6.0f, 0.0f, 3.0f)); 
-        glm::mat4 earthMVP = projection * view * earthModel;
+        earthModel = glm::rotate(earthModel, earthOrbitAngle, glm::vec3(0.0f, 1.0f, 0.0f));
+        earthModel = glm::translate(earthModel, glm::vec3(6.0f, 0.0f, 0.0f)); 
+        earthModel = glm::rotate(earthModel, earthSpinAngle, glm::vec3(0.0f, 1.0f, 0.0f)); 
         
+        glm::mat4 earthMVP = projection * view * earthModel;
         glUniformMatrix4fv(mvpLoc, 1, GL_FALSE, glm::value_ptr(earthMVP));
         glUniform3f(colorLoc, 0.2f, 0.5f, 1.0f); 
         earth.draw();
 
         glfwSwapBuffers(window);
         glfwPollEvents();
+    }
     }
     }
     glfwTerminate();
