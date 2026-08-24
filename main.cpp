b12114fc4aeda310485309dd127bcb58fcd608e2
Ind0Pacific <vashishtdeepanshu87@gmail.com>
2026-08-24 21:38:18 +0530

    Added real time planet spawning.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include "Common.h"
 #include "Shader.h"
 #include "Sphere.h"
-#include "orbit.h"
 #include "Physics.h"
 #include "Starfield.h"
 #include <glm/gtc/type_ptr.hpp>
@@ -38,9 +37,17 @@ int main()
     unsigned int colorLoc = glGetUniformLocation(shaderProgram, "color");
 
     Sphere unitSphere(36, 18, 1.0f);
-    Orbit orbitRing(100);
     Starfield stars(5000);
     initSolarSystem();
+
+    unsigned int trailVAO, trailVBO;
+    glGenVertexArrays(1, &trailVAO);
+    glGenBuffers(1, &trailVBO);
+    glBindVertexArray(trailVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, trailVBO);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(glm::vec3), (void*)0);
+    glEnableVertexAttribArray(0);
+    glBindVertexArray(0);
 
     float simulatedTime = 0.0f;
     const float EARTH_YEAR_DURATION = 4.0212f;
@@ -60,6 +67,27 @@ int main()
         lastFrame = currentFrame;
         processInput(window);
 
+        static bool spaceWasPressed = false;
+        bool spaceIsPressed = glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS;
+
+        if (spaceIsPressed && !spaceWasPressed) {
+            float randomMass = 0.5f + ((float)rand() / RAND_MAX) * 5.0f; 
+            float randomRadius = 0.3f + (randomMass * 0.1f);
+            
+            glm::vec3 randomColor = glm::vec3(
+                (float)rand() / RAND_MAX, 
+                (float)rand() / RAND_MAX, 
+                (float)rand() / RAND_MAX
+            );
+
+            float shootSpeed = 20.0f; 
+            glm::vec3 spawnVel = cameraFront * shootSpeed;
+            float randomSpin = -3.0f + ((float)rand() / RAND_MAX) * 6.0f;
+
+            addPlanet(cameraPos, spawnVel, randomMass, randomRadius, randomColor, randomSpin);
+        }
+        spaceWasPressed = spaceIsPressed;
+
         glClearColor(0.02f, 0.02f, 0.02f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glUseProgram(shaderProgram);
@@ -71,55 +99,45 @@ int main()
         }
 
         int currentYear = static_cast<int>(simulatedTime / EARTH_YEAR_DURATION);
-
         std::string title = "Solar System Simulation | Year: " + std::to_string(currentYear) +
-                            " | Movement: W, A, S, D | Exit: ESC";
-
+                            " | Movement: W, A, S, D |Planet Spawn: Spacebar | Exit: ESC";
         glfwSetWindowTitle(window, title.c_str());
 
         int currentWidth, currentHeight;
         glfwGetFramebufferSize(window, &currentWidth, &currentHeight);
-        if (currentHeight == 0)
-            currentHeight = 1;
+        if (currentHeight == 0) currentHeight = 1;
         float aspectRatio = (float)currentWidth / (float)currentHeight;
 
         glm::mat4 projection = glm::perspective(glm::radians(45.0f), aspectRatio, 0.1f, 1000.0f);
         glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
-        glm::vec3 sunPos = planets[0].pos;
 
         glm::mat4 starModel = glm::mat4(1.0f);
         glm::mat4 starMVP = projection * view * starModel;
         glUniformMatrix4fv(mvpLoc, 1, GL_FALSE, glm::value_ptr(starMVP));
-        
-
         glUniform3f(colorLoc, 1.0f, 1.0f, 1.0f); 
         stars.draw();
- 
 
-        for (size_t i = 1; i < planets.size(); ++i) {
-            const auto& planet = planets[i];
+        glm::mat4 lineMVP = projection * view; 
+        glUniformMatrix4fv(mvpLoc, 1, GL_FALSE, glm::value_ptr(lineMVP));
+        glBindVertexArray(trailVAO);
+
+        for (size_t i = 1; i < planets.size(); ++i)
+        {
+            if (planets[i].trail.size() < 2) continue;
+
+            glBindBuffer(GL_ARRAY_BUFFER, trailVBO);
+            glBufferData(GL_ARRAY_BUFFER, planets[i].trail.size() * sizeof(glm::vec3), planets[i].trail.data(), GL_DYNAMIC_DRAW);
+
+            glm::vec3 trailColor = planets[i].color * 0.5f;
+            glUniform3fv(colorLoc, 1, glm::value_ptr(trailColor));
             
-            glm::vec3 orbitCenter = sunPos;
-            if (i == 4) {
-                orbitCenter = planets[3].pos;
-            }
-            float distance = glm::distance(planet.pos, orbitCenter);
-            
-            glm::mat4 model = glm::mat4(1.0f);
-            model = glm::translate(model, orbitCenter);
-            model = glm::scale(model, glm::vec3(distance)); 
-
-            glm::mat4 mvp = projection * view * model;
-            glUniformMatrix4fv(mvpLoc, 1, GL_FALSE, glm::value_ptr(mvp));
-            glUniform3f(colorLoc, 0.2f, 0.2f, 0.2f); 
-
-            orbitRing.draw();
+            glDrawArrays(GL_LINE_STRIP, 0, planets[i].trail.size());
         }
+        glBindVertexArray(0);
 
         for (const auto &planet : planets)
         {
             glm::mat4 model = glm::mat4(1.0f);
-
             model = glm::translate(model, planet.pos);
             model = glm::rotate(model, glm::radians(20.0f), glm::vec3(0.0f, 0.0f, 1.0f));
             model = glm::rotate(model, planet.currentAngle, glm::vec3(0.0f, 1.0f, 0.0f));
@@ -131,9 +149,13 @@ int main()
 
             unitSphere.draw();
         }
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+    glDeleteVertexArrays(1, &trailVAO);
+    glDeleteBuffers(1, &trailVBO);
     glfwTerminate();
     return 0;
 }
